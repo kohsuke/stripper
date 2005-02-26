1d280fbe25204f2a9a550a8283a3b95b9f1f8261
Kohsuke Kawaguchi <kk@kohsuke.org>
2005-02-26 17:00:45 +0000

    implemented alias support.

--- src/org/kohsuke/stripper/InfoSetBuilder.java
+++ src/org/kohsuke/stripper/InfoSetBuilder.java
@@ -13,6 +13,15 @@ public class InfoSetBuilder {
         if(name.equalsIgnoreCase("all"))
             r = EnumSet.allOf(Info.class);
         else
+        if(name.equals("basicDebugInfo"))
+            r.addAll(BASIC_DEBUG_INFO);
+        else
+        if(name.equals("allDebugInfo"))
+            r.addAll(ALL_DEBUG_INFO);
+        else
+        if(name.equals("compilerInfo"))
+            r.addAll(COMPILER_INFO);
+        else
             r.add(Info.valueOf(name));
     }
 
@@ -23,4 +32,27 @@ public class InfoSetBuilder {
     Set<Info> getResult() {
         return r;
     }
+    
+    private static final Set<Info> BASIC_DEBUG_INFO = EnumSet.of(
+            Info.LocalVariableTable,
+            Info.LocalVariableTypeTable
+    );
+
+    private static final Set<Info> ALL_DEBUG_INFO = EnumSet.of(
+            Info.LocalVariableTable,
+            Info.LocalVariableTypeTable,
+            Info.LineNumberTable,
+            Info.SourceFile
+    );
+
+    private static final Set<Info> COMPILER_INFO = EnumSet.of(
+            Info.Deprecated,
+            Info.Synthetic,
+            Info.InnerClasses,
+            Info.EnclosingMethod,
+            Info.RuntimeInvisibleAnnotations,
+            Info.RuntimeInvisibleParameterAnnotations,
+            Info.Signature,
+            Info.SourceDebugExtension
+    );
 }
